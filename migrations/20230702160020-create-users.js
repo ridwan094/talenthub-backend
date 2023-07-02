d6eee05d62bcbfa4f9b52d0d5e40129d585c817b
Diky Arif Febriyanto <dikyariff97@gmail.com>
2023-07-02 23:06:34 +0700

    Add Table Users

--- migrations/20230702160020-create-users.js
+++ migrations/20230702160020-create-users.js
@@ -2,34 +2,21 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Talents', {
+    await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      talent_name: {
+      email: {
         type: Sequelize.STRING
       },
-      talent_image_path: {
+      password: {
         type: Sequelize.STRING
       },
-      talent_summary: {
-        type: Sequelize.TEXT
-      },
-      work_since: {
-        type: Sequelize.DATE
-      },
-      education: {
-        type: Sequelize.TEXT
-      },
-      cv_file_path: {
-        type: Sequelize.STRING
-      },
-      working_status: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false,
+      role: {
+        type: Sequelize.ENUM(`admin`, `user`)
       },
       createdAt: {
         allowNull: false,
@@ -42,6 +29,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Talents');
+    await queryInterface.dropTable('users');
   }
 };
